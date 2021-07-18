780780a50472cfcecfea6b5ff1e2d5eac0087add
Divya Rajput <divya.rajput8220@gmail.com>
2021-07-18 18:55:29 +0530

    My first program

---
+++ Numeric.go
@@ -0,0 +1,7 @@
+package main
+
+import "fmt"
+
+func myMethod() {
+	fmt.Println("Holaaa!! I am being called by main method.")
+}
--- main.go
+++ main.go
@@ -3,5 +3,6 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hey World!!!")
+	fmt.Println("Hey There!!!")
+	myMethod()
 }
